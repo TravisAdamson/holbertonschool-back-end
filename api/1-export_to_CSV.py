5ceac35a608915de4293039a573abb1df5b90f3b
Travis Adamson <tndadamson@gmail.com>
2023-11-16 08:38:51 -0600

    Adding file 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Requests the user information from a given API """
+import requests
+
+
+def display_employee_progress(employee_id):
+    """ script must display to stdout the employee todo list progress """
+    url = "https://jsonplaceholder.typicode.com"
+    empl_url = f"{url}/users/{employee_id}"
+    todo_url = f"{url}/todos"
+
+    empl_data = requests.get(empl_url).json()
+    todo_data = requests.get(todo_url,
+                             params={"userId": employee_id}).json()
+
+    empl_name = empl_data.get("name")
+    completed_tasks = [t["title"] for t in todo_data if t["completed"]]
+    num_done = len(completed_tasks)
+    num_total = len(todo_data)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(empl_name, num_done, num_total))
+    for task in completed_tasks:
+        print(f"\t {task}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    display_employee_progress(int(sys.argv[1]))
